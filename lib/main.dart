0a46eec81197eb1eab1c021c71e70c2eee525703
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-05-11 02:42:04 +0530

    firebase communication for android established

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ShareACab',
+      title: 'Share A Cab',
       builder: (context, child) {
         return MediaQuery(
           child: child,
