a91868ef714f903e7d2560265cc8ccb7489c26e1
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-05-11 22:27:20 +0530

    improved layouts

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shareacab/screens/authenticate/forgotpass.dart';
 import 'package:shareacab/services/auth.dart';
+import 'package:shareacab/shared/constants.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -41,57 +42,62 @@ class _SignInState extends State<SignIn> {
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.isEmpty ? 'enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) =>
-                    val.length < 6 ? 'enter a pass >6 chars' : null,
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                  validator: (val) => val.isEmpty ? 'enter an email' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result =
-                        await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() => error = 'couldnt sign in');
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration:
+                      textInputDecoration.copyWith(hintText: 'Password'),
+                  validator: (val) =>
+                      val.length < 6 ? 'enter a pass >6 chars' : null,
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.signInWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() => error = 'couldnt sign in');
+                      }
                     }
-                  }
-                },
-              ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
-                  'Forgot Password',
-                  style: TextStyle(color: Colors.white),
+                  },
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/accounts/forgotpass');
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-            ],
+                RaisedButton(
+                  color: Colors.blue,
+                  child: Text(
+                    'Forgot Password',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/accounts/forgotpass');
+                  },
+                ),
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
+                ),
+              ],
+            ),
           ),
         ),
       ),
