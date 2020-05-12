1504e66f350b960fb3a5b5456a26646b49fdfd0e
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-05-12 06:31:18 +0530

    fixed error handling

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,65 +3,32 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // create user object based on firebaseuser
-
-  // User _userFromFirebaseUser(FirebaseUser user) {
-  //   return user != null ? User(uid: user.uid) : null;
-  // }
-
   // auth change user stream
 
   Stream<FirebaseUser> get user {
     return _auth.onAuthStateChanged;
-    //.map((FirebaseUser user) => _userFromFirebaseUser(user));
-    //.map(_userFromFirebaseUser);
   }
 
-  //sign in anonymously
-  // Future signInAnon() async {
-  //   try {
-  //     AuthResult result = await _auth.signInAnonymously();
-  //     FirebaseUser user = result.user;
-  //     return _userFromFirebaseUser(user);
-  //   } catch (e) {
-  //     print(e.toString());
-  //     return null;
-  //   }
-  // }
-
   //sign in with email pass
-  Future signInWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      if (result.user.isEmailVerified) {
-        return result.user.uid;
-      } else {
-        print('Not verified');
-        return null;
-      }
-    } catch (e) {
-      print(e.toString());
-      return null;
+
+  Future<bool> signInWithEmailAndPassword(String email, String password) async {
+    AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    if (result.user.isEmailVerified) {
+      return true;
+    } else {
+      await result.user.sendEmailVerification();
+      return false;
     }
   }
 
   // sign up with email pass
 
-  Future registerWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      try {
-        await result.user.sendEmailVerification();
-        return result.user.uid;
-      } catch (e) {
-        print('couldnt send mail');
-      }
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
+  Future<void> registerWithEmailAndPassword(
+      String email, String password) async {
+    AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    await result.user.sendEmailVerification();
   }
 
   // forgot password
@@ -71,12 +38,7 @@ class AuthService {
   }
 
   // sign out
-  Future signOut() async {
-    try {
-      return await _auth.signOut();
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
+  Future<void> signOut() async {
+    await _auth.signOut();
   }
 }
