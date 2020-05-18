d7789a01fd4581425551d32d8725e37f2e0fb73c
deepanshu <deepanshu38841@gmail.com>
2020-05-18 12:52:56 +0530

    Added animations and suggestions

--- lib/screens/messages.dart
+++ lib/screens/messages.dart
@@ -12,13 +12,75 @@ class _MessagesState extends State<Messages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: !isSearching ? Text('Messages') : TextField(decoration: InputDecoration(hintText: 'Search'),), iconButton: IconButton(icon: Icon(Icons.search), onPressed: (){
-      setState(() {
-        isSearching = !isSearching;
-      });
-      }),),
+        appBar: MyAppBar(title:  Text('Messages'), iconButton: IconButton(icon: Icon(Icons.search), onPressed: (){
+          showSearch(context: context, delegate: DataSearch());
+        }),),
 
-      body:  ChatPage()
+        body:  ChatPage()
     );
   }
+}
+
+class DataSearch extends SearchDelegate{
+
+  final chats =[
+    'Arpit Sir',
+    'Vishal Sir',
+    'Shashwat Sir',
+    'Ishaan',
+    'Kshitij'
+  ];
+  final recent =[
+      'Shashwat Sir',
+    'Kshitij'
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(icon: Icon(Icons.clear), onPressed: (){
+      query = '';
+      })
+    ];
+
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+  return IconButton(icon: AnimatedIcon(icon: AnimatedIcons.menu_arrow, progress: transitionAnimation), onPressed: (){
+  close(context, null);
+  });
+
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty ? recent : chats;
+    return ListView.builder(itemBuilder: (context, index)=>
+      ListTile(leading: Icon(Icons.person),
+      title: Text(suggestionList[index]),),
+        itemCount: suggestionList.length,
+    );
+
+  }
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme.copyWith(
+      primaryColor: Theme.of(context).primaryColor,
+      primaryIconTheme: theme.primaryIconTheme,
+      primaryColorBrightness: Theme.of(context).primaryColorBrightness,
+      primaryTextTheme: Theme.of(context).primaryTextTheme,
+    );
+  }
+
 }
