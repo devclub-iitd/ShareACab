04644570b665fea7f13ef2013575909a61afe7cb
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-05-19 15:41:12 +0530

    fix lint

--- lib/screens/userprofile.dart
+++ lib/screens/userprofile.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shareacab/screens/edituserdetails.dart';
 import 'package:shareacab/shared/loading.dart';
 
 class MyProfile extends StatefulWidget {
