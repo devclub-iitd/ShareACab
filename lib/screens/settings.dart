6a014b55fbdd2cfcae24619a40ee977f6cb235ea
Deepanshu Rohilla <deepanshu38841@gmail.com>
2020-06-17 17:55:30 +0530

    removed logout from settings if user is not logged in

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -4,6 +4,8 @@ import 'package:shareacab/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shareacab/services/auth.dart';
 import 'package:progress_dialog/progress_dialog.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Settings extends StatefulWidget {
   final AuthService _auth;
@@ -19,10 +21,42 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     final themeNotifier = Provider.of<ThemeNotifier>(context);
+    final user = Provider.of<FirebaseUser>(context);
     _darkTheme = (themeNotifier.getTheme() == darkTheme);
     return Scaffold(
         appBar: AppBar(
           title: Text('Settings'),
+          actions: <Widget>[
+            user != null
+                ? FlatButton.icon(
+                    textColor: getVisibleColorOnPrimaryColor(context),
+                    icon: Icon(FontAwesomeIcons.signOutAlt),
+                    onPressed: () async {
+                      ProgressDialog pr;
+                      pr = ProgressDialog(context, type: ProgressDialogType.Normal, isDismissible: false, showLogs: false);
+                      pr.style(
+                        message: 'Logging out...',
+                        backgroundColor: Theme.of(context).backgroundColor,
+                        messageTextStyle: TextStyle(color: Theme.of(context).accentColor),
+                      );
+                      await pr.show();
+                      await Future.delayed(Duration(seconds: 1)); // sudden logout will show ProgressDialog for a very short time making it not very nice to see :p
+                      try {
+                        await widget._auth.signOut();
+                        await pr.hide();
+                        Navigator.pop(context);
+                      } catch (err) {
+                        // show e.message
+                        await pr.hide();
+                        String errStr = err.message ?? err.toString();
+                        final snackBar = SnackBar(content: Text(errStr), duration: Duration(seconds: 3));
+                        scaffoldKey.currentState.showSnackBar(snackBar);
+                      }
+                    },
+                    label: Text('Logout'),
+                  )
+                : FlatButton(onPressed: null, child: null)
+          ],
         ),
         body: ListView(
           children: <Widget>[
@@ -45,38 +79,6 @@ class _SettingsState extends State<Settings> {
                     onThemeChanged(val, themeNotifier);
                   },
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            ListTile(
-              title: Text(
-                'Logout',
-                style: TextStyle(fontSize: 28.0),
-              ),
-              contentPadding: EdgeInsets.all(26.0),
-              onTap: () async {
-                ProgressDialog pr;
-                pr = ProgressDialog(context, type: ProgressDialogType.Normal, isDismissible: false, showLogs: false);
-                pr.style(
-                  message: 'Logging out...',
-                  backgroundColor: Theme.of(context).backgroundColor,
-                  messageTextStyle: TextStyle(color: Theme.of(context).accentColor),
-                );
-                await pr.show();
-                await Future.delayed(Duration(seconds: 1)); // sudden logout will show ProgressDialog for a very short time making it not very nice to see :p
-                try {
-                  await widget._auth.signOut();
-                  await pr.hide();
-                } catch (err) {
-                  // show e.message
-                  await pr.hide();
-                  String errStr = err.message ?? err.toString();
-                  final snackBar = SnackBar(content: Text(errStr), duration: Duration(seconds: 3));
-                  scaffoldKey.currentState.showSnackBar(snackBar);
-                }
-              },
-              trailing: Transform.scale(
-                scale: 1.6,
               ),
             ),
           ],
