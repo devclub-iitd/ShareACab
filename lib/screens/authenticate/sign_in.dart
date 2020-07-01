cbfa2b061f7d0e6a7716aba4f4a269cfb216a6ff
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-07-01 19:35:29 +0530

    added Snackbar and Progressdialog in auth screens

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+// import 'package:provider/provider.dart';
 import 'package:shareacab/main.dart';
 import 'package:shareacab/screens/settings.dart';
 import 'package:shareacab/services/auth.dart';
@@ -16,7 +17,8 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  var _darkTheme = true;
+  //var _darkTheme = true;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   bool loading = false;
@@ -36,11 +38,12 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    final themeNotifier = Provider.of<ThemeNotifier>(context);
-    _darkTheme = (themeNotifier.getTheme() == darkTheme);
+    // final themeNotifier = Provider.of<ThemeNotifier>(context);
+    // _darkTheme = (themeNotifier.getTheme() == darkTheme);
     return loading
         ? Loading()
         : Scaffold(
+            key: _scaffoldKey,
             backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             appBar: AppBar(
               backgroundColor: Theme.of(context).primaryColor,
@@ -71,108 +74,125 @@ class _SignInState extends State<SignIn> {
                 ),
               ],
             ),
-            body: GestureDetector(
-              onTap: () {
-                FocusScope.of(context).unfocus();
-              },
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-                child: Form(
-                  key: _formKey,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        SizedBox(height: 20.0),
-                        CircleAvatar(
-                          radius: 48,
-                          backgroundColor: Theme.of(context).accentColor,
-                          child: Icon(
-                            CupertinoIcons.car_detailed,
-                            size: 48,
-                          ),
-                        ),
-                        SizedBox(height: 10.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'ShareACab ',
-                              style: TextStyle(
-                                fontFamily: 'Poiret',
-                                fontSize: 47,
-                                color: Theme.of(context).accentColor,
-                                fontWeight: FontWeight.bold,
-                                textBaseline: TextBaseline.alphabetic,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 20.0),
-                        TextFormField(
-                          decoration: InputDecoration(hintText: 'Email', enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: getBorderColorForInputFields(context), width: 2.0)), focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2.0))),
-                          validator: (val) => val.isEmpty ? 'Enter a valid Email' : null,
-                          onChanged: (val) {
-                            setState(() => email = val);
-                          },
-                        ),
-                        SizedBox(height: 20.0),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: getBorderColorForInputFields(context), width: 2.0)),
-                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2.0)),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                passwordHide ? Icons.visibility_off : Icons.visibility,
-                                color: Theme.of(context).accentColor,
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  passwordHide = !passwordHide;
-                                });
-                              },
+            body: Builder(builder: (BuildContext context) {
+              return GestureDetector(
+                onTap: () {
+                  FocusScope.of(context).unfocus();
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                  child: Form(
+                    key: _formKey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          SizedBox(height: 20.0),
+                          CircleAvatar(
+                            radius: 48,
+                            backgroundColor: Theme.of(context).accentColor,
+                            child: Icon(
+                              CupertinoIcons.car_detailed,
+                              size: 48,
                             ),
                           ),
-                          validator: (val) => val.length < 6 ? 'Enter a password greater than 6 characters.' : null,
-                          obscureText: passwordHide,
-                          onChanged: (val) {
-                            setState(() => password = val);
-                          },
-                        ),
-                        SizedBox(height: 20.0),
-                        RaisedButton(
-                          color: Theme.of(context).accentColor,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16),
-                            child: Text(
-                              'SIGN IN',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Theme.of(context).scaffoldBackgroundColor,
-                                fontWeight: FontWeight.w500,
-                                letterSpacing: 3,
+                          SizedBox(height: 10.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'ShareACab ',
+                                style: TextStyle(
+                                  fontFamily: 'Poiret',
+                                  fontSize: 47,
+                                  color: Theme.of(context).accentColor,
+                                  fontWeight: FontWeight.bold,
+                                  textBaseline: TextBaseline.alphabetic,
+                                ),
                               ),
-                            ),
+                            ],
                           ),
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              setState(() => loading = true);
-                              try {
-                                setState(() {
-                                  email = email.trim();
-                                });
-
-                                var flag = await _auth.signInWithEmailAndPassword(email, password);
-                                if (flag == false) {
+                          SizedBox(height: 20.0),
+                          TextFormField(
+                            decoration: InputDecoration(hintText: 'Email', enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: getBorderColorForInputFields(context), width: 2.0)), focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2.0))),
+                            validator: (val) => val.isEmpty ? 'Enter a valid Email' : null,
+                            onChanged: (val) {
+                              setState(() => email = val);
+                            },
+                          ),
+                          SizedBox(height: 20.0),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: getBorderColorForInputFields(context), width: 2.0)),
+                              focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2.0)),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  passwordHide ? Icons.visibility_off : Icons.visibility,
+                                  color: Theme.of(context).accentColor,
+                                ),
+                                onPressed: () {
                                   setState(() {
-                                    error = 'ID not verified, verification mail sent again.';
+                                    passwordHide = !passwordHide;
                                   });
-                                }
-                                setState(() => loading = false);
-                              } catch (e) {
-                                if (mounted) {
-                                  setState(() {
+                                },
+                              ),
+                            ),
+                            validator: (val) => val.length < 6 ? 'Enter a password greater than 6 characters.' : null,
+                            obscureText: passwordHide,
+                            onChanged: (val) {
+                              setState(() => password = val);
+                            },
+                          ),
+                          SizedBox(height: 20.0),
+                          RaisedButton(
+                            color: Theme.of(context).accentColor,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: Text(
+                                'SIGN IN',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Theme.of(context).scaffoldBackgroundColor,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 3,
+                                ),
+                              ),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                //setState(() => loading = true);
+
+                                ProgressDialog pr;
+                                pr = ProgressDialog(context, type: ProgressDialogType.Normal, isDismissible: false, showLogs: false);
+                                pr.style(
+                                  message: 'Signing in...',
+                                  backgroundColor: Theme.of(context).backgroundColor,
+                                  messageTextStyle: TextStyle(color: Theme.of(context).accentColor),
+                                );
+                                await pr.show();
+                                await Future.delayed(Duration(seconds: 1));
+                                try {
+                                  email = email.trim();
+
+                                  var flag = await _auth.signInWithEmailAndPassword(email, password);
+                                  if (flag == false) {
+                                    error = 'ID not verified, verification mail sent again.';
+                                    Scaffold.of(context).hideCurrentSnackBar();
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                      backgroundColor: Theme.of(context).primaryColor,
+                                      duration: Duration(seconds: 2),
+                                      content: Text(
+                                        error,
+                                        style: TextStyle(color: Theme.of(context).accentColor),
+                                      ),
+                                    ));
+                                  }
+                                  await pr.hide();
+                                  //setState(() => loading = false);
+                                } catch (e) {
+                                  await pr.hide();
+                                  if (mounted) {
                                     switch (e.code) {
                                       case 'ERROR_INVALID_EMAIL':
                                         error = 'Your email address appears to be malformed.';
@@ -198,53 +218,57 @@ class _SignInState extends State<SignIn> {
                                           error = 'An undefined Error happened.';
                                         }
                                     }
-                                    loading = false;
-
-                                    // Scaffold.of(context).showSnackBar(
-                                    //     SnackBar(content: Text(error)));
-                                  });
+                                    //loading = false;
+                                    Scaffold.of(context).hideCurrentSnackBar();
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                      backgroundColor: Theme.of(context).primaryColor,
+                                      duration: Duration(seconds: 2),
+                                      content: Text(
+                                        error,
+                                        style: TextStyle(color: Theme.of(context).accentColor),
+                                      ),
+                                    ));
+                                  }
                                 }
                               }
-                            }
-                          },
-                        ),
-                        SizedBox(height: 20.0),
-                        RaisedButton(
-                          color: Theme.of(context).accentColor,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16),
-                            child: Text(
-                              'FORGOT PASSWORD',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Theme.of(context).scaffoldBackgroundColor,
-                                letterSpacing: 2,
-                                fontWeight: FontWeight.w500,
+                            },
+                          ),
+                          SizedBox(height: 20.0),
+                          RaisedButton(
+                            color: Theme.of(context).accentColor,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: Text(
+                                'FORGOT PASSWORD',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Theme.of(context).scaffoldBackgroundColor,
+                                  letterSpacing: 2,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
                             ),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/accounts/forgotpass');
+                            },
                           ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/accounts/forgotpass');
-                          },
-                        ),
-                        SizedBox(height: 20.0),
-                        !_darkTheme
-                            ? Text(
-                                'Pro-Tip: Enable Dark mode from settings (icon in the AppBar).',
-                                style: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic, color: Colors.green),
-                              )
-                            : Text(''),
-                        SizedBox(height: 12.0),
-                        Text(
-                          error,
-                          style: TextStyle(color: Colors.red, fontSize: 14.0),
-                        ),
-                      ],
+                          SizedBox(height: 20.0),
+                          Text(
+                            'Tip: Toggle theme from settings (icon in the AppBar).',
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic, color: Theme.of(context).accentColor),
+                          ),
+                          SizedBox(height: 12.0),
+                          // Text(
+                          //   error,
+                          //   style: TextStyle(color: Colors.red, fontSize: 14.0),
+                          // ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          );
+              );
+            }));
   }
 }
