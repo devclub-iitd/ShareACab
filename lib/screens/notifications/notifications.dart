c73103b42c75e5622096786c5fab7c81301fafae
Deepanshu Rohilla <deepanshu38841@gmail.com>
2020-07-10 12:22:18 +0530

    remove all button given flat button widget

--- lib/screens/notifications/notifications.dart
+++ lib/screens/notifications/notifications.dart
@@ -15,24 +15,12 @@ class _NotificationsState extends State<Notifications> {
       appBar: AppBar(
         title: Text('Notifications'),
         actions: <Widget>[
-          GestureDetector(
-            onTap: () async {
-              await _notifServices.removeAll();
-            },
-            child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  border: Border.all(color: Colors.white),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.delete),
-                    Text('Remove All'),
-                  ],
-                )),
-          )
+          FlatButton.icon(
+              onPressed: () async {
+                await _notifServices.removeAll();
+              },
+              icon: Icon(Icons.delete),
+              label: Text('Remove All'))
         ],
       ),
       body: Container(
