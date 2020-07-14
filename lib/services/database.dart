66ad20c1b68dd6be41d9d98d7edae3e7c8a60f8a
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-07-14 17:44:42 +0530

    database code cleaned

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -79,28 +79,6 @@ class DatabaseService {
     return userDetails.document(uid).snapshots();
   }
 
-  // add request details from user (will use in future versions)
-  Future<void> addRequest(RequestDetails requestDetails) async {
-    var user = await _auth.currentUser();
-
-    // CODE FOR CONVERTING DATE TIME TO TIMESTAMP
-
-    var temp = requestDetails.startTime;
-    var starting = DateTime(requestDetails.startDate.year, requestDetails.startDate.month, requestDetails.startDate.day, temp.hour, temp.minute);
-    var temp2 = requestDetails.endTime;
-    var ending = DateTime(requestDetails.endDate.year, requestDetails.endDate.month, requestDetails.endDate.day, temp2.hour, temp2.minute);
-
-    await requests.add({
-      'user': user.uid.toString(),
-      'destination': requestDetails.destination.toString(),
-      'start': starting,
-      'end': ending,
-      'finaldestination': requestDetails.finalDestination.toString(),
-      'maxpoolers': 0,
-      'created': Timestamp.now(),
-    });
-  }
-
   // add group details (W = 4, R = 0)
   Future<void> createTrip(RequestDetails requestDetails) async {
     var user = await _auth.currentUser();
@@ -112,17 +90,6 @@ class DatabaseService {
     var temp2 = requestDetails.endTime;
     var ending = DateTime(requestDetails.endDate.year, requestDetails.endDate.month, requestDetails.endDate.day, temp2.hour, temp2.minute);
 
-    //var timeStamp = Timestamp.fromDate(temp2);
-
-    // final reqRef = await requests.add({
-    //   'user': user.uid.toString(),
-    //   'destination': requestDetails.destination.toString(),
-    //   'start': starting,
-    //   'end': ending,
-    //   'finaldestination': requestDetails.finalDestination.toString(),
-    //   'maxpoolers': requestDetails.maxPoolers,
-    //   'created': Timestamp.now(),
-    // });
     final docRef = await groupdetails.add({
       'owner': user.uid.toString(),
       'users': FieldValue.arrayUnion([user.uid]),
@@ -141,8 +108,6 @@ class DatabaseService {
 
     await userDetails.document(user.uid).updateData({
       'currentGroup': docRef.documentID,
-      // 'currentReq': reqRef.documentID,
-      //'previous_groups': FieldValue.arrayUnion([docRef.documentID]),
     });
 
     var request = groupdetails.document(docRef.documentID).collection('users');
@@ -188,7 +153,6 @@ class DatabaseService {
     if (startTimeStamp.compareTo(Timestamp.now()) > 0) {
       await userDetails.document(user.uid).updateData({
         'currentGroup': null,
-        //'currentReq': null,
         'cancelledRides': cancelledRides + 1,
       });
       await groupdetails.document(currentGrp).updateData({
@@ -212,7 +176,6 @@ class DatabaseService {
     else {
       await userDetails.document(user.uid).updateData({
         'currentGroup': null,
-        //'currentReq': null,
         'totalRides': totalRides + 1,
         'previous_groups': FieldValue.arrayUnion([currentGrp]),
       });
@@ -229,14 +192,12 @@ class DatabaseService {
     var user = await _auth.currentUser();
     var presentNum;
     await userDetails.document(user.uid).updateData({
-      // 'previous_groups': FieldValue.arrayUnion([listuid]),
       'currentGroup': listuid,
     });
     await groupdetails.document(listuid).get().then((value) {
       presentNum = value.data['numberOfMembers'];
     });
     await groupdetails.document(listuid).updateData({
-      // presently storing user.uid in users, need to change this later to the requestID when we start taking input of requests. (IMPORTANT)
       'users': FieldValue.arrayUnion([user.uid.toString()]),
       'numberOfMembers': presentNum + 1,
     });
