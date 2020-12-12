716b720314381427ebb5beb41d2aaf9d44577318
rish07 <rishirajsinghchhabra@gmail.com>
2020-12-12 12:59:05 +0530

    Add timestamp in message bubbles on chat screen

--- lib/screens/chatscreen/chat_widgets/chat_bubble.dart
+++ lib/screens/chatscreen/chat_widgets/chat_bubble.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.message, this.username, this.isMe, {this.key});
+  MessageBubble(this.message, this.username, this.isMe, {this.key, this.time});
 
   @override
   final Key key;
+  final String time;
   final String username;
   final String message;
   final bool isMe;
@@ -14,43 +15,85 @@ class MessageBubble extends StatelessWidget {
     return Stack(
       children: [
         Row(
-          mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: isMe ? Colors.grey : Theme.of(context).accentColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(12),
-                  topRight: Radius.circular(12),
-                  bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-                  bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: isMe ? Colors.grey : Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                      bottomLeft:
+                          !isMe ? Radius.circular(0) : Radius.circular(12),
+                      bottomRight:
+                          isMe ? Radius.circular(0) : Radius.circular(12),
+                    ),
+                  ),
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    left: 16,
+                    right: 16,
+                    bottom: 16,
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.6,
+                    minWidth: MediaQuery.of(context).size.width * 0.3,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        username,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: isMe
+                              ? Colors.black
+                              : Theme.of(context)
+                                  .accentTextTheme
+                                  .subtitle1
+                                  .color,
+                        ),
+                      ),
+                      Text(
+                        message,
+                        style: TextStyle(
+                          color: isMe
+                              ? Colors.black
+                              : Theme.of(context)
+                                  .accentTextTheme
+                                  .subtitle1
+                                  .color,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width*0.6,
-                minWidth: MediaQuery.of(context).size.width*0.3,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    username,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: isMe ? Colors.black : Theme.of(context).accentTextTheme.subtitle1.color,
-                    ),
+                Positioned(
+                  bottom: 14,
+                  right: 14,
+                  child: Row(
+                    children: [
+                      Text(
+                        time,
+                        style: TextStyle(
+                          color: isMe
+                              ? Colors.black
+                              : Theme.of(context)
+                                  .accentTextTheme
+                                  .subtitle1
+                                  .color,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    message,
-                    style: TextStyle(
-                      color: isMe ? Colors.black : Theme.of(context).accentTextTheme.subtitle1.color,
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
