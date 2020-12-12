5d12b0342f15108ac2d419905a53b9a16985805d
Rishi Raj Singh <49035175+rish07@users.noreply.github.com>
2020-12-12 17:22:52 +0530

    Add timestamp in message bubbles on chat screen (#161)

--- lib/screens/chatscreen/chat_widgets/message.dart
+++ lib/screens/chatscreen/chat_widgets/message.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shareacab/screens/chatscreen/chat_widgets/chat_bubble.dart';
 
 class MessageScreen extends StatelessWidget {
@@ -36,10 +37,21 @@ class MessageScreen extends StatelessWidget {
                 );
               }
               final chatDocs = chatSnapshot.data.documents;
+
               return ListView.builder(
                 reverse: true,
                 itemCount: chatDocs.length,
-                itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text'], chatDocs[index]['name'], chatDocs[index]['userId'] == futureSnapshot.data.uid, key: ValueKey(chatDocs[index].documentID)),
+                itemBuilder: (ctx, index) => MessageBubble(
+                  chatDocs[index]['text'],
+                  chatDocs[index]['name'],
+                  chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                  key: ValueKey(chatDocs[index].documentID),
+                  time: DateFormat().add_jm().format(
+                        DateTime.parse(
+                          chatDocs[index]['createdAt'].toDate().toString(),
+                        ),
+                      ),
+                ),
               );
             });
       },
